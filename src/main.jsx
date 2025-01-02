9bb9e58872193850a44694c8d2e08d87cc486c88
hosenA51 <hosenahmed2552@gmail.com>
2025-01-02 21:48:41 +0600

    added Our Menu page

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,14 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import { router } from './Routes/Routes';
+import { HelmetProvider } from 'react-helmet-async';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <div>
-      <RouterProvider router={router} />
-    </div>
+    <HelmetProvider>
+      <div>
+        <RouterProvider router={router} />
+      </div>
+    </HelmetProvider>
   </StrictMode>,
 )
