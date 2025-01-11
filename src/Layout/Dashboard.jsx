5d20da8a0cb52cea81121ba19a5f045075d2566a
hosenA51 <hosenahmed2552@gmail.com>
2025-01-11 13:06:04 +0600

    added manageItems page in dashboard

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -29,7 +29,7 @@ const Dashboard = () => {
                                 </NavLink>
                             </li>
                             <li>
-                                <NavLink to="/dashboard/mangeItems">
+                                <NavLink to="/dashboard/manageItems">
                                     <FaList></FaList>
                                    Manage Items
                                 </NavLink>
